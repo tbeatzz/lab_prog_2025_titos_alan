6755b9816ec061465bdc43d28a67802d49d62528
titosalan <tbeatzz20k@gmail.com>
2025-05-21 00:34:55 -0300

    maquetacion parte 1 final

--- public/app/js/item/edit.js
+++ public/app/js/item/edit.js
@@ -6,8 +6,8 @@ const deleteButton = document.getElementById('deleteButton');
 const editButton = document.getElementById('editButton')
 
 const cambiarEdicion = (habilitar) => {
-    const form = document.getElementById('editUserForm');
-    const elementos = form.querySelectorAll('input, select');
+    const form = document.getElementById('editProductForm');
+    const elementos = form.querySelectorAll('input, select, textarea');
 
     elementos.forEach(el => {
         el.disabled = !habilitar;
