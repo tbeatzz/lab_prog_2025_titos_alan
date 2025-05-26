e8abcadac6185e3779f39d3a4555670680cbcaf5
titosalan <tbeatzz20k@gmail.com>
2025-05-26 08:37:27 -0300

    parte 2 - gestion de usuarios

--- public/app/js/item/edit.js
+++ public/app/js/item/edit.js
@@ -1,28 +1,28 @@
-const updateButton = document.getElementById('updateButton');
-const cancelButton = document.getElementById('cancelButton');
-const exportButton = document.getElementById('exportButton');
-const deleteButton = document.getElementById('deleteButton');
+// const updateButton = document.getElementById('updateButton');
+// const cancelButton = document.getElementById('cancelButton');
+// const exportButton = document.getElementById('exportButton');
+// const deleteButton = document.getElementById('deleteButton');
 
-const editButton = document.getElementById('editButton')
+// const editButton = document.getElementById('editButton')
 
-const cambiarEdicion = (habilitar) => {
-    const form = document.getElementById('editProductForm');
-    const elementos = form.querySelectorAll('input, select, textarea');
+// const cambiarEdicion = (habilitar) => {
+//     const form = document.getElementById('editProductForm');
+//     const elementos = form.querySelectorAll('input, select, textarea');
 
-    elementos.forEach(el => {
-        el.disabled = !habilitar;
-    });
+//     elementos.forEach(el => {
+//         el.disabled = !habilitar;
+//     });
 
-    // con t saca la clase y con f agrega la clase
-    updateButton.classList.toggle('d-none', !habilitar); 
-    cancelButton.classList.toggle('d-none', !habilitar); 
+//     // con t saca la clase y con f agrega la clase
+//     updateButton.classList.toggle('d-none', !habilitar); 
+//     cancelButton.classList.toggle('d-none', !habilitar); 
     
-    deleteButton.disabled = habilitar;
-    exportButton.disabled = habilitar;
-    editButton.disabled = habilitar;
+//     deleteButton.disabled = habilitar;
+//     exportButton.disabled = habilitar;
+//     editButton.disabled = habilitar;
 
     
-};
+// };
 
-const activarEdicion = () => cambiarEdicion(true);
-const cancelarEdicion = () => cambiarEdicion(false); 
+// const activarEdicion = () => cambiarEdicion(true);
+// const cancelarEdicion = () => cambiarEdicion(false); 
