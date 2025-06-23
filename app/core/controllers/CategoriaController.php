46a00653c3ebd132d644a5bf08356c757f15176d
titos alan notebook <tbeatzz20k@gmail.com>
2025-06-23 14:31:07 -0300

    controllers

---
+++ app/core/controllers/CategoriaController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\core\controllers;
+
+use app\libs\http\Request;
+use app\libs\http\Response;
+
+final class CategoriaContoller extends BaseController implements InterfaceController{
+    
+    public function index(Request $request, Response $response): void{
+
+    }
+
+    public function load(Request $request, Response $response): void{
+
+    }
+
+    public function create(Request $request, Response $response): void{
+
+    }
+    
+    public function save(Request $request, Response $response): void{
+
+    }
+
+    public function edit(Request $request, Response $response): void{
+
+    }
+
+    public function update(Request $request, Response $response): void{
+
+    }
+
+    public function delete(Request $request, Response $response): void{
+
+    }
+
+    public function list(Request $request, Response $response): void{
+
+    }
+
+}
